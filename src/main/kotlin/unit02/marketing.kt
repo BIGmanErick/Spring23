e07bf31a5344bc6f00e892960e693c5228578c3f
BIGmanErick <erick.aguinaga03@gmail.com>
2023-02-16 17:00:53 -0600

    If...else

---
+++ src/main/kotlin/unit02/marketing.kt
@@ -0,0 +1,13 @@
+package unit02
+
+fun main(args: Array<String>) {
+    var friendsCount: Int = 500
+    if ( friendsCount > 499 ) {
+        println("Adopt a Dog Today!")
+    }
+    else {
+        println("Buy Cat Food At 20% Off.")
+    }
+
+}
+
