911df5882c72f8c5592745fc0e663bd18464cc64
BIGmanErick <erick.aguinaga03@gmail.com>
2023-02-16 17:44:56 -0600

    Salary

---
+++ src/main/kotlin/unit02/salary.kt
@@ -0,0 +1,10 @@
+package unit02
+
+fun main() {
+    var name : String = "Erick Aguinaga"
+    var age : Int = 21
+    var annualPay : Double = 80000.0
+
+    println("My name is $name, my age is $age and I hope to earn $$annualPay per year." )
+
+}
